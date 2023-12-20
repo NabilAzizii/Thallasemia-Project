d3a60bcca6886f500f21f391c31059416698b566
NabilAzizii <nabilazizi109@gmail.com>
2023-12-20 19:35:42 +0700

    Improve Model

--- app.py
+++ app.py
@@ -2,25 +2,24 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import json
+import time
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing import image as keras_image
 from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
-# Load the model and class mapping outside the ClassifyModel class
-model = load_model("model_thalassemia_classifier_80.h5")
+model = load_model("model_thalassemia_classifier_100.h5")
 with open("class.json") as fin:
     tag2class = json.load(fin)
     class2tag = {v: k for k, v in tag2class.items()}
 
-# Create an instance of ClassifyModel
 class ClassifyModel:
     def __init__(self):
         self.model = None
         self.class2tag = None
         self.tag2class = None
 
-    def load(self, model_path="model_thalassemia_classifier_80.h5", class_path="class.json"):
+    def load(self, model_path="model_thalassemia_classifier_100.h5", class_path="class.json"):
         self.model = load_model(model_path)
         with open(class_path) as fin:
             self.tag2class = json.load(fin)
@@ -35,7 +34,7 @@ class ClassifyModel:
 m = ClassifyModel()
 m.load()
 
-st.title('Thallasemia Classification')
+st.title('Thalassemia Classification')
 
 st.header("Upload a blood image")
 
@@ -43,6 +42,7 @@ st.sidebar.title("About")
 
 st.sidebar.info(
     "An application that utilizes artificial intelligence to accurately classify thalassemia disease through blood image analysis. Using advanced machine learning models, we are committed to providing innovative solutions that aid early diagnosis and effective management of this disease.")
+
 
 st.sidebar.title("Creator")
 
@@ -56,10 +56,13 @@ st.sidebar.info(
     "Muhammad Nabil Azizi - 120450090"
 )
 
+st.sidebar.title("Source")
+st.sidebar.info("[Link to Original Repository](https://github.com/sains-data/Thalassemia-Classification-CNN.git)")
+
 uploaded_file = st.file_uploader("Choose a blood image...", type=['jpeg', 'jpg', 'png'])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).resize((80, 80))
+    image = Image.open(uploaded_file).resize((100, 100))
     img_array = np.array(image)
     img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
@@ -69,8 +72,9 @@ if uploaded_file is not None:
     st.image(image, use_column_width=True, caption=f'Uploaded Image: {uploaded_file.name}')
 
     if st.button('Predict'):
+        start_time = time.time()
         pred_digits, pred_probabilities = m.predict(img_array)
-
+        end_time = time.time()
         if len(pred_digits) > 0 and pred_digits[0] < len(class2tag):
             predicted_label = class2tag[pred_digits[0]]
             confidence = pred_probabilities[0][pred_digits[0]]
@@ -78,3 +82,8 @@ if uploaded_file is not None:
             st.write(f"Confidence: **{round(float(confidence), 4) * 100}%**")
         else:
             st.write("Error: Predicted class index out of bounds.")
+        execution_time = end_time - start_time
+        st.write(f"Time taken for prediction: **{execution_time:.4f} seconds**")
+        
+
+
